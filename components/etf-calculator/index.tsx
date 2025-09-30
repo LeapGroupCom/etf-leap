4372e04b7d8d7ac1e176c2e6eb57ff4c9926b4b5
vo1mer <volodymyr.khomovyi@gmail.com>
2025-09-30 20:46:51 +0200

    add try block for etf fetch

--- components/etf-calculator/index.tsx
+++ components/etf-calculator/index.tsx
@@ -4,23 +4,32 @@ import { EtfCalculatorClient } from './etf-calculator-client'
 import { EtfCalcStoreProvider } from './provider'
 
 async function getEstimates(ticker: string) {
-	const { summaryDetail, fundPerformance, fundProfile } = await yahooFinance.quoteSummary(ticker, {
-		modules: ['summaryDetail', 'fundPerformance', 'fundProfile'],
-	})
+	try {
+		const { summaryDetail, fundPerformance, fundProfile } = await yahooFinance.quoteSummary(ticker, {
+			modules: ['summaryDetail', 'fundPerformance', 'fundProfile'],
+		})
 
-	const yieldRaw = summaryDetail?.yield ?? 0
-	const estDividendYield = Number((yieldRaw * 100).toFixed(2))
+		const yieldRaw = summaryDetail?.yield ?? 0
+		const estDividendYield = Number((yieldRaw * 100).toFixed(2))
 
-	const returnRaw = fundPerformance?.trailingReturns.tenYear ?? 0
-	const estTotalReturn = Number((returnRaw * 100).toFixed(2))
+		const returnRaw = fundPerformance?.trailingReturns.tenYear ?? 0
+		const estTotalReturn = Number((returnRaw * 100).toFixed(2))
 
-	const expenseRatioRaw = fundProfile?.feesExpensesInvestment?.annualReportExpenseRatio ?? 0
-	const expenseRatio = Number((expenseRatioRaw * 100).toFixed(2))
+		const expenseRatioRaw = fundProfile?.feesExpensesInvestment?.annualReportExpenseRatio ?? 0
+		const expenseRatio = Number((expenseRatioRaw * 100).toFixed(2))
 
-	return {
-		estDividendYield,
-		estTotalReturn,
-		expenseRatio,
+		return {
+			estDividendYield,
+			estTotalReturn,
+			expenseRatio,
+		}
+	} catch (error) {
+		console.error(error)
+		return {
+			estDividendYield: 0,
+			estTotalReturn: 0,
+			expenseRatio: 0,
+		}
 	}
 }
 
@@ -33,14 +42,7 @@ export const EtfCalculator = async ({ ticker }: { ticker: string }) => {
 	})
 
 	return (
-		<EtfCalcStoreProvider
-			serverSideData={{
-				estTotalReturn: estTotalReturn ?? 0,
-				estDividendYield: estDividendYield ?? 0,
-				ticker,
-				expenseRatio: expenseRatio ?? 0,
-			}}
-		>
+		<EtfCalcStoreProvider serverSideData={{ estTotalReturn, estDividendYield, ticker, expenseRatio }}>
 			<EtfCalculatorClient />
 		</EtfCalcStoreProvider>
 	)
